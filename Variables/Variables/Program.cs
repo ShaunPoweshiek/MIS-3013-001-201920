42db1f3615e6f27e2403089c8d8232fbaece5717
powe0072 <powe0072@PCB-LAB360-08>
2020-01-22 12:55:02 -0600

    Created variables for assignment

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -11,6 +11,24 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //Create a string named firstName
+            string firstName;
+
+
+            //Assign a value to firstName (use your name)
+            firstName = "Shaun";
+
+            //Create an int named favoriteNumber and assign your favorite number to it
+            int favoriteNumber = 7;
+
+            double crazyNumber = 7.6767 * favoriteNumber;
+
+            string lastName = "Poweshiek";
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine(fullName + " has the favorite number of " + favoriteNumber + " \nwhich equates to a crazy number of " + crazyNumber);
+
+            Console.ReadKey();
         }
     }
 }
