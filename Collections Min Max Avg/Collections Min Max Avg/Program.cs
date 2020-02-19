1575785790a619e8c2ef9c9d71cce7c6b0ed15c2
powe0072 <powe0072@PCB-LAB360-08>
2020-02-19 13:14:42 -0600

    End of class save point.

---
+++ Collections Min Max Avg/Collections Min Max Avg/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Min_Max_Avg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Shaun Poweshiek, worked with Robert Young & Author Kudhlamde
+
+
+            
+            string examInput = "Y";
+            
+
+            do
+            {
+                
+                Console.Write("Enter your test score?");
+                double testScore = Convert.ToDouble(Console.ReadLine());
+                
+
+
+                Console.WriteLine("Do you have another exam to add? (Y=Yes,N=No)");
+                string examInputAgain = Console.ReadLine();
+            }
+            while (examInput == "Y");
+
+
+            List<double> testScore = new List<double>();
+
+
+            /*int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 5);*/
+        }
+    }
+}
