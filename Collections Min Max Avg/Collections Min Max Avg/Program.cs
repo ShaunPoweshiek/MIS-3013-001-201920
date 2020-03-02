2ba6aa7659e4d4781f0e5a76b912d359f9dbcc88
powe0072 <powe0072@PCB-LAB360-08>
2020-03-02 13:14:47 -0600

    Save.

--- Collections Min Max Avg/Collections Min Max Avg/Program.cs
+++ Collections Min Max Avg/Collections Min Max Avg/Program.cs
@@ -13,24 +13,70 @@ namespace Collections_Min_Max_Avg
 
 
             
-            string examInput = "Y";
-            
+            string examInput = "Y", answer;
+            List<double> scores = new List<double>();
 
             do
             {
                 
                 Console.Write("Enter your test score?");
-                double testScore = Convert.ToDouble(Console.ReadLine());
-                
+                examScore = Console.ReadLine();
+                double es = Convert.ToDouble(examScore);
+                scores.Add(es);
+                //scores.Add(Convert.ToDouble(Console.ReadLine()));
 
 
                 Console.WriteLine("Do you have another exam to add? (Y=Yes,N=No)");
-                string examInputAgain = Console.ReadLine();
+                answer = Console.ReadLine();
             }
-            while (examInput == "Y");
+            while (answer.ToLower == "Y");
 
 
-            List<double> testScore = new List<double>();
+            double sum = 0; //this is often referred to as an ACCUMULATOR variable
+            double minimum = scores[0];
+            double maximum = scores[0];
+
+            for (int i = 0; i < scores.Count; i++)
+            {
+                double score = scores[i];
+                sum = sum + score;
+
+                if (score < minimum)
+                {
+                    minimum = score;
+                }
+                if (score > maximum)
+                {
+                    maximum = score;
+                }
+            }
+
+            foreach (var score in scores)
+            {
+                sum = sum + score;
+
+                if (score < minimum)
+                {
+                    minimum = score;
+                }
+                if (score > maximum)
+                {
+                    maximum = score;
+                }
+            }
+
+
+            double average = sum / scores.Count;
+
+            Console.WriteLine($"Your average for the {scores.Count} exam grades is {average.ToString("N3")}");
+            Console.WriteLine($"Your minimum exam grades is {minimum.ToString("N3")}");
+            Console.WriteLine($"Your maximum exam grades is {maximum.ToString("N3")}");
+
+
+
+            Console.ReadKey();
+
+            
 
 
             /*int i = 0;
---
+++ Collections Min Max Avg/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Shaun Poweshiek, worked with Robert Young, Author Kudhlamde, Cale
+
+        //Part 1
+        //string[] classes = { "MIS", "MATH", "MGT", "FIN" };
+        //string[] courseNum = { "3013", "3033", "3013", "2303" };
+
+        string[] classes = new string[3];
+        string[] courseNum = new string[3];
+
+            for (int i = 0; i < classes.Length; i++)
+            {
+                Console.WriteLine("Please enter your course name:");
+                classes[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < courseNum.Length; i++)
+            {
+                Console.WriteLine("Please enter your course number:");
+                courseNum[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < classes.Length; i++)
+            {
+                Console.WriteLine(string.Concat(classes[i].ToUpper(), courseNum[i].ToUpper()));
+            }
+
+            //Part 2
+            List<double> scores = new List<double>();
+
+            do
+            {
+
+                Console.Write("Enter your test score?");
+                examScore = Console.ReadLine();
+                double es = Convert.ToDouble(examScore);
+                scores.Add(es);
+                //scores.Add(Convert.ToDouble(Console.ReadLine()));
+
+
+                Console.WriteLine("Do you have another exam to add? (Y=Yes,N=No)");
+                answer = Console.ReadLine();
+            }
+            while (answer.ToLower == "Y");
+
+
+            Console.ReadKey();
+        }
+    }
+}
